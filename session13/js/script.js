d7c5fa3d5d92809fe3bb466877d2bbab22486c00
NouranFayez <nouran_ahmed269@hotmail.com>
2026-07-21 14:30:47 +0300

    session13

--- session13/js/script.js
+++ session13/js/script.js
@@ -22,10 +22,10 @@
 ////////////////////////////////////
 
 let obj = {
-    name : "Tamer",
-    age : 10 , 
+    name: "Tamer",
+    age: 10,
     address: "Maadi",
-    isGraduated : false
+    isGraduated: false
 }
 
 ////////// destructuring
@@ -51,31 +51,31 @@ let obj = {
 
 let str = "         Lorem ipsum dolor sit amet consectetur, consectetur elit. Porro, ipsa.              "
 
-console.log(str.length)
-console.log(str[0])
+// console.log(str.length)
+// console.log(str[0])
 
-for(let i = 0 ; i < str.length ; i++){
+for (let i = 0; i < str.length; i++) {
     // console.log(str[i])
 }
 
-console.log(str.includes('amet'))
-console.log(str.includes('jbnjbj'))
-console.log(str.indexOf("ipsum"))
-console.log(str.replace("consectetur" , "Javascript"))
-console.log(str.replaceAll("consectetur" , "Javascript"))
-console.log(str)
-console.log(str.trim())
-console.log(str.slice(20 , 30))
+// console.log(str.includes('amet'))
+// console.log(str.includes('jbnjbj'))
+// console.log(str.indexOf("ipsum"))
+// console.log(str.replace("consectetur" , "Javascript"))
+// console.log(str.replaceAll("consectetur" , "Javascript"))
+// console.log(str)
+// console.log(str.trim())
+// console.log(str.slice(20 , 30))
 
 // let courses = "Html,Css,Js"
 // console.log(courses.split(","))
 
 let courses = "Html Css Js"
-console.log(courses.split(" "))
-console.log(courses.startsWith("Html"))
-console.log(courses.startsWith("gggg"))
-console.log(courses.endsWith("gggg"))
-console.log(courses.endsWith("Js"))
+// console.log(courses.split(" "))
+// console.log(courses.startsWith("Html"))
+// console.log(courses.startsWith("gggg"))
+// console.log(courses.endsWith("gggg"))
+// console.log(courses.endsWith("Js"))
 
 //////////////////////////////////////////////////
 
@@ -115,39 +115,110 @@ console.log("------------------------")
 
 let users = [
     {
-        name : "Tamer",
-        id:1,
-        balance:1000
+        name: "Tamer",
+        id: 1,
+        balance: 1000
     },
-        {
-        name : "Sara",
-        id:2,
-        balance:1000
+    {
+        name: "Sara",
+        id: 2,
+        balance: 1000
     },
-        {
-        name : "Omar",
-        id:3,
-        balance:1000
+    {
+        name: "Omar",
+        id: 3,
+        balance: 1000
     },
 ]
 
-localStorage.setItem('users' , JSON.stringify(users))
-console.log(JSON.stringify(users))
+localStorage.setItem('users', JSON.stringify(users))
+// console.log(JSON.stringify(users))
 
-console.log("---------------------------------")
+// console.log("---------------------------------")
 
-let newList = JSON.parse(localStorage.getItem('users')) 
+let newList = JSON.parse(localStorage.getItem('users'))
 
-console.log(newList)
+// console.log(newList)
 
-newList.push({name : 'vghj' , id:3 , balance:777})
+newList.push({ name: 'vghj', id: 3, balance: 777 })
 
 
 localStorage.removeItem('Name')
 
-localStorage.setItem("users" , JSON.stringify(newList))
+localStorage.setItem("users", JSON.stringify(newList))
 
-console.log(localStorage.getItem('address'))
+// console.log(localStorage.getItem('address'))
+
+////////////////////////////////////////////////////////
+
+// DOM ==> document object model
+
+
+// document.getElementsByTagName("div").test = "dfgh" 
+
+// console.log(document.getElementsByTagName("div")) // return HTMLCollection (special object)
+
+
+let elements = document.getElementsByClassName("bg-primary") // return HTMLCollection (special object)
+// console.log(elements)
+// elements[1].innerText = "jS"
+
+// elements.forEach((ele)=>{
+//     console.log(ele)
+// })
+
+// let div = document.getElementById('div-style') // return object
+// console.log(div)
+// console.log(div.id)
+// console.log(div.innerText)
+// div.innerText = "Javascript"
+
+// .card
+let query = document.querySelector("p.card") // return object
+console.log(query)
+query.innerText = "Paragraph"
+
+let queryAll = document.querySelectorAll('p.card') // return nodelist (special object)
+queryAll.test = "sdfghj"
+console.log(queryAll)
+
+// queryAll[0].innerText = "dfghjkl"
+// queryAll[1].innerText = "dfghjkl"
+
+// for(let i = 0 ; i < queryAll.length ; i++){
+//     queryAll[i].innerText = "sadfghj dfghjkl dfghjkl fghj"
+// }
+
+// queryAll.forEach((element)=>{
+//         element.innerText = "Web Design"
+// })
+
+
+let h1 = document.querySelector('#h1-id')
+// h1.style.color = "teal"
+// h1.style.backgroundColor = "lightgray"
+// h1.style.padding = "3%"
+// h1.style.width = "40%"
+// h1.style.margin = "2% auto"
+
+console.log(h1.className)
+
+// h1.className = "bg-primar border custom-class"
+
+h1.classList.add('custom-class')
+h1.classList.remove('bg-primary')
+h1.classList.toggle('border-primary')
+
+
+
+let changeImg = () => {
+    let img = document.querySelector('img')
+
+    console.log(img.src)
+    img.src = "images/2.avif"
+}
+
+
 
 
 
